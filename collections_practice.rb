83d4a588fc78b46dded7bc8083bca8864339acd1
Doug Jones <dsjr1284@gmail.com>
2020-03-15 20:40:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,44 @@
+def sort_array_asc(integers) 
+      integers.sort
+end 
 
+def sort_array_desc(integers)
+  integers.sort do|a, b|
+    b <=> a
+  end 
+end 
+
+def sort_array_char_count(array)
+  array.sort do|a, b|
+    a.length <=> b.length
+  end 
+end
+
+def swap_elements(array)
+array[1], array[2] = array[2], array[1]
+   array
+end 
+
+def reverse_array(integers)
+  integers.reverse
+end 
+
+def kesha_maker(array)
+     array.collect do |string|
+       string[2] = "$" 
+     end 
+     array
+end 
+
+def find_a(array)
+  array.select {|str| str.start_with?("a")}
+end 
+
+def sum_array(array)
+  
+end 
+
+# def add_s(array)
+  
+# end 
+  
